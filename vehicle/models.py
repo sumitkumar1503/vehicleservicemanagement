75f0bae41ce471b516b672716ad5eb05c0489415
sumit <wapka1503@gmail.com>
2020-10-02 06:15:23 +0530

    increased category

--- vehicle/models.py
+++ vehicle/models.py
@@ -34,7 +34,7 @@ class Mechanic(models.Model):
 
 
 class Request(models.Model):
-    cat=(('two wheeler','two wheeler'),('three wheeler','three wheeler'))
+    cat=(('two wheeler with gear','two wheeler with gear'),('two wheeler without gear','two wheeler without gear'),('three wheeler','three wheeler'),('four wheeler','four wheeler'))
     category=models.CharField(max_length=50,choices=cat)
 
     vehicle_no=models.PositiveIntegerField(null=False)
