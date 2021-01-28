d15d27b65418770d8a5915a93f86d92e44aa1be3
sumit <wapka1503@gmail.com>
2021-01-28 08:50:42 +0530

    mysql to sqlite

--- vehicleservicemanagement/settings.py
+++ vehicleservicemanagement/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'vehicleservicemanagement.wsgi.application'
 
 DATABASES = {
  'default': {
-     'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'vehicle1',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+     'ENGINE': 'django.db.backends.sqlite3',
+     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
  }
 }
 
